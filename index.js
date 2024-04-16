953cd4dc6826d23e953cb6a3e38cd3ae0999eb20
Jorge Silva <jorgesilva@Jorges-MacBook-Pro.local>
2024-04-16 19:01:54 +0200

    mobile side bar

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function toogleClass() {
+    console.log('clicked')
+    document.getElementsByClassName("hamburger-menu")[0].classList.toggle('open')
+    document.getElementsByClassName("sidebar")[0].classList.toggle('open')
+}
