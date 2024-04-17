7bf027036aa671a76260a2c9a7f151b8844e8e72
Jorge Silva <jorgesilva@Jorges-MacBook-Pro.local>
2024-04-17 18:17:33 +0200

    adding animation

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
 function toogleClass() {
-    console.log('clicked')
+    console.log("Here")
     document.getElementsByClassName("hamburger-menu")[0].classList.toggle('open')
     document.getElementsByClassName("sidebar")[0].classList.toggle('open')
+    
+    document.getElementsByClassName("hamburger-menu")[0].classList.toggle('active');
+    document.getElementsByClassName("hamburger-menu")[0].classList.toggle('not-active');
 }
+
